5bf0d5ae250451305ee74f4513a9c78b67011add
iniad <cornforprogramming@gmail.com>
2022-10-07 12:04:08 +0900

    update template file

--- .prac/template.py
+++ .prac/template.py
@@ -5,19 +5,19 @@ def main():
         return sys.stdin.readline().rstrip()
     
     # 整数 1 つ
-    N = int(input())
+    # N = int(input())
 
     # 整数複数個
-    N, K = map(int, input().split())
+    # N, K = map(int, input().split())
 
     # 整数 N 個 (改行区切り)
-    L = [int(input()) for i in range(N)]
+    # L = [int(input()) for i in range(N)]
 
     # 整数 N 個 (スペース区切り)
-    A = list(map(int, input().split()))
+    # A = list(map(int, input().split()))
 
     # 整数 (縦 N 横 複数 の行列)
-    S = [list(map(int, input().split())) for _ in range(N)]
+    # S = [list(map(int, input().split())) for _ in range(N)]
 
 if __name__ == '__main__':
     main()
